1b2067b53e06f2a3ddabae1050452b6eb269c242
daehyuk1231 <daehyuk1231@naver.com>
2026-01-07 17:15:20 +0900

    11286_절댓값힙

--- src/BaekJoon/BaekJoon_11286_절댓값힙.java
+++ src/BaekJoon/BaekJoon_11286_절댓값힙.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
 
-public class BaekJoon_11268_절댓값힙 {
+public class BaekJoon_11286_절댓값힙 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
