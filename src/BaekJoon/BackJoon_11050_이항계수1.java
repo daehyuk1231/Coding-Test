c9bb43791d6a3463f931b1865fc02ac28100cbe7
daehyuk1231 <daehyuk1231@naver.com>
2026-01-20 21:30:23 +0900

    이항계수1

---
+++ src/BaekJoon/BackJoon_11050_이항계수1.java
@@ -0,0 +1,27 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BackJoon_11050_이항계수1 {
+    static int N, K;
+    static int D[][];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        K = sc.nextInt();
+        D = new int[11][11];
+
+        for(int i = 0; i<=N; i++) {
+            D[i][i] = 1;
+            D[i][0] = 1;
+            D[i][1] = i;
+        }
+
+        for(int i = 2; i<=N; i++) {
+            for(int j = 1; j < i; j++) {
+                D[i][j] = D[i-1][j] + D[i-1][j-1];
+            }
+        }
+        System.out.println(D[N][K]);
+    }
+}
