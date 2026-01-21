6bc142385a3e415c713e7f3aa6f7f40424562ef8
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 14:38:36 +0900

    고양이

---
+++ src/BaekJoon/BackJoon_10171_고양이.java
@@ -0,0 +1,10 @@
+package BaekJoon;
+
+public class BackJoon_10171_고양이 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
