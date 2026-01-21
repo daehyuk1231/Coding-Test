0a49a729540ee12259880378ad6f79f9a92f3710
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 13:28:45 +0900

    A-B

--- src/BaekJoon/BackJoon_1001_AB.java
+++ src/BaekJoon/BackJoon_1001_AB.java
@@ -2,11 +2,11 @@ package BaekJoon;
 
 import java.util.Scanner;
 
-public class BackJoon_1000_AplusB {
+public class BackJoon_1001_AB {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int A = sc.nextInt();
         int B = sc.nextInt();
-        System.out.println(A+B);
+        System.out.println(A-B);
     }
 }
