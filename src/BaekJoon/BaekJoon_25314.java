e02e764d4120c82e275a9b5f1d56fbd3eaf8bcaf
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 17:22:24 +0900

    25314

---
+++ src/BaekJoon/BaekJoon_25314.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int repeat = N / 4;
+        for (int i = 0; i < repeat; i++) {
+            System.out.print("long ");
+        }
+        System.out.println("int");
+    }
+}
