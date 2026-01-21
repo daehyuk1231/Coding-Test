78d21b738372f3a65fd3261d0b3ffa629b1d1961
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 13:32:39 +0900

    Welovekriii

---
+++ src/BaekJoon/BackJoon_10718_Welovekriii.java
@@ -0,0 +1,8 @@
+package BaekJoon;
+
+public class BackJoon_10718_Welovekriii {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
