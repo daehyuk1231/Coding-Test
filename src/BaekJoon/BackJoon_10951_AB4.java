2143dd46674dd1f593f8f726f1ee86e08496dd72
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 15:24:53 +0900

    AB4

---
+++ src/BaekJoon/BackJoon_10951_AB4.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BackJoon_10951_AB4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(sc.hasNext()) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+            System.out.println(A+B);
+        }
+    }
+}
