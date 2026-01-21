be60365e5075292fb180482d180f0b985179c32e
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 15:29:38 +0900

    아스키코드

---
+++ src/BaekJoon/BackJoon_11654_아스키코드.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BackJoon_11654_아스키코드 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String text =  sc.next();
+        char textCtr = text.charAt(0);
+        System.out.println((int)textCtr);
+    }
+}
