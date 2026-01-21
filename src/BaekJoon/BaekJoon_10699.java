d5bac9bdabaa82ce4e687d7667a72b438636f8c7
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 17:39:31 +0900

    10699

---
+++ src/BaekJoon/BaekJoon_10699.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class BaekJoon_10699 {
+    public static void main(String[] args) {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(simpleDateFormat.format(date));
+    }
+}
