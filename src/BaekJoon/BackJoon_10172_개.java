94be642cb70fb9ab6b058779e1bd5109e0615964
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 14:40:20 +0900

    개

---
+++ src/BaekJoon/BackJoon_10172_개.java
@@ -0,0 +1,11 @@
+package BaekJoon;
+
+public class BackJoon_10172_개 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/|\n" +
+                "|q p|   /}\n" +
+                "( 0 )\"\"\"\\\n" +
+                "|\"^\"`    |\n" +
+                "||_/=\\\\__|");
+    }
+}
