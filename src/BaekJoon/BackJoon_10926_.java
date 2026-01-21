9dccd7e99a24aeb9b48fe77f27111ad0831196a9
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 15:33:51 +0900

    10926

---
+++ src/BaekJoon/BackJoon_10926_.java
@@ -0,0 +1,11 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BackJoon_10926_ {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        System.out.println(id+"??!");
+    }
+}
