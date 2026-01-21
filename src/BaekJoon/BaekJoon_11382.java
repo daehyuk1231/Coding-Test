e4837394b236813a411b789395177cc4c8c24d54
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 16:45:36 +0900

    11382

---
+++ src/BaekJoon/BaekJoon_11382.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_11382 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A+B+C);
+    }
+}
