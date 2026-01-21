4f648ab9e8a615cbf35406d8a58cc283822f626d
daehyuk1231 <daehyuk1231@naver.com>
2026-01-21 14:45:16 +0900

    윤년

---
+++ src/BaekJoon/BackJoon_2753_윤년.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BackJoon_2753_윤년 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int year = sc.nextInt();
+
+        if((year%4==0 && year%100!=0) || year%400==0) {
+            System.out.println("1");
+        } else {
+            System.out.println("0");
+        }
+    }
+}
