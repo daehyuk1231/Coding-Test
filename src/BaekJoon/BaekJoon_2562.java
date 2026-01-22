349be65c1e7033b3e44d459e1de1256ada663fa4
daehyuk1231 <daehyuk1231@naver.com>
2026-01-22 16:32:44 +0900

    2562

---
+++ src/BaekJoon/BaekJoon_2562.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_2562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int max_index = 0;
+        for (int i = 1; i <= 9; i++) {
+            int temp = sc.nextInt();
+            if (temp > max) {
+                max = temp;
+                max_index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.println(max_index);
+    }
+}
