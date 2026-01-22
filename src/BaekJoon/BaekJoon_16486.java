20d318c394df50debc722d9b09b9c452d1c1730d
daehyuk1231 <daehyuk1231@naver.com>
2026-01-22 15:25:47 +0900

    16486

---
+++ src/BaekJoon/BaekJoon_16486.java
@@ -0,0 +1,13 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_16486 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt();
+        int d2 = sc.nextInt();
+        double pie = 3.141592;
+        System.out.println(d1 * 2 + 2 * pie * d2);
+    }
+}
