717a840ba55bdb34429c2ba411022a6c9289f0b0
daehyuk1231 <daehyuk1231@naver.com>
2026-01-22 16:23:02 +0900

    2884

---
+++ src/BaekJoon/BaekJoon_2884.java
@@ -0,0 +1,19 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_2884 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int H = sc.nextInt();
+        int M = sc.nextInt();
+
+        if (M < 45) {
+            M += 60;
+            if (H == 0) H = 23;
+            else H--;
+        }
+        M = M - 45;
+        System.out.println(H + " " + M);
+    }
+}
