a03c52b474d000c7098a963203fe619fb59b268a
daehyuk1231 <daehyuk1231@naver.com>
2026-01-22 15:37:49 +0900

    4299

---
+++ src/BaekJoon/BaekJoon_4299.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_4299 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        if (B > A) System.out.println(-1);
+        else {
+            int X = (A + B) / 2;
+            int Y = (A - B) / 2;
+            if (X + Y == A && X - Y == B) {
+                System.out.println(X + " " + Y);
+            } else {
+                System.out.println(-1);
+            }
+        }
+    }
+}
