8a896fbda240caedc054cacd6e924e68a029358f
daehyuk1231 <daehyuk1231@naver.com>
2026-01-23 19:34:18 +0900

    3053

---
+++ src/BaekJoon/BaekJoon_3053.java
@@ -0,0 +1,12 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_3053 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double R = sc.nextDouble();
+        System.out.println(Math.PI*R*R);
+        System.out.println(2*R*R);
+    }
+}
