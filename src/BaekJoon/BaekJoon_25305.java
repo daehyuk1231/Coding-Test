0afc569ee1f4a5f4eb5ad164d74d3f17fb39811b
daehyuk1231 <daehyuk1231@naver.com>
2026-01-26 21:52:04 +0900

    25305

---
+++ src/BaekJoon/BaekJoon_25305.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BaekJoon_25305 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] A = new int[n];
+        for (int i = 0; i < n; i++) A[i] = sc.nextInt();
+        Arrays.sort(A);
+        System.out.println(A[A.length - k]);
+    }
+}
