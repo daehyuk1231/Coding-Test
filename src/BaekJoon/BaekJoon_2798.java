d6ed0513b088b7dbfcdee64b4bf321d80efca518
daehyuk1231 <daehyuk1231@naver.com>
2026-01-26 16:51:44 +0900

    2798

---
+++ src/BaekJoon/BaekJoon_2798.java
@@ -0,0 +1,31 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_2798 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int Max = 0;
+        for (int i = 0; i < N; i++) {
+            for (int j = i + 1; j < N; j++) {
+                for (int k = j + 1; k < N; k++) {
+                    int temp = A[i] + A[j] + A[k];
+                    if (temp > Max && M >= temp) Max = temp;
+                }
+            }
+        }
+        System.out.println(Max);
+    }
+}
