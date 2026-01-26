2f55394b22a11ce0a11f5ab2ece3fce51e0290aa
daehyuk1231 <daehyuk1231@naver.com>
2026-01-26 15:09:22 +0900

    5565

---
+++ src/BaekJoon/BaekJoon_5565.java
@@ -0,0 +1,14 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_5565 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = sc.nextInt();
+        for (int i = 0; i < 9; i++) {
+            total -= sc.nextInt();
+        }
+        System.out.println(total);
+    }
+}
