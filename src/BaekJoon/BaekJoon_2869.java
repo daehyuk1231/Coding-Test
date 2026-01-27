62106e2da6911c0b151d383ab26661afc37f12d8
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 12:17:42 +0900

    2869

---
+++ src/BaekJoon/BaekJoon_2869.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_2869 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int V = sc.nextInt();
+
+        int C = A - B;
+        int day = (V - B) / C;
+        if ((V - B) % C != 0) day++;
+
+        System.out.println(day);
+    }
+}
