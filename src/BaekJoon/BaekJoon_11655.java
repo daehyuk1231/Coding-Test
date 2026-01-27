1b29f1bda892f9be0bfeb501fcbc9afc85cb522d
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 16:45:45 +0900

    11655

---
+++ src/BaekJoon/BaekJoon_11655.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_11655 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] text = sc.nextLine().toCharArray();
+        for (int i = 0; i < text.length; i++) {
+            char now = text[i];
+            if ('a' <= now && now <= 'z') {
+                now = (char) (now + 13);
+                if (now > 'z') {
+                    now = (char) (now % 122 + 96);
+                }
+            } else if ('A' <= now && now <= 'Z') {
+                now = (char) (now + 13);
+                if (now > 'Z') {
+                    now = (char) (now % 90 + 64);
+                }
+            }
+            System.out.print(now);
+        }
+    }
+}
