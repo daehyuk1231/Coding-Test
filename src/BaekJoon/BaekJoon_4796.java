77926006a2cc8e6d182782bff6e9841d37009833
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 17:48:01 +0900

    4796

---
+++ src/BaekJoon/BaekJoon_4796.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_4796 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int cnt = 0;
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int L = Integer.parseInt(st.nextToken());
+            int P = Integer.parseInt(st.nextToken());
+            int V = Integer.parseInt(st.nextToken());
+            if (L == 0 && P == 0 && V == 0) break;
+            int day = V / P * L;
+            day += Math.min(L, V % P);
+            cnt++;
+            System.out.println("Case " + cnt + ": " + day);
+        }
+    }
+}
