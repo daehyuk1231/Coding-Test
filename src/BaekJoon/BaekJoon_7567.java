8d7b888828207a00e296ff02c8363195a16768cb
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 10:58:59 +0900

    7567

---
+++ src/BaekJoon/BaekJoon_7567.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_7567 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char[] A = sc.nextLine().toCharArray();
+        int sum = 10;
+        char before = A[0];
+
+        for (int i = 1; i < A.length; i++) {
+            char now = A[i];
+            if (before == now) sum += 5;
+            else sum += 10;
+            before = now;
+        }
+        System.out.println(sum);
+    }
+}
