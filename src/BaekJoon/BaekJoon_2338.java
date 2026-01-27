61e2a3dff9b37093cda6e3c206db7e9a6270883b
daehyuk1231 <daehyuk1231@naver.com>
2026-01-28 00:34:25 +0900

    2338

---
+++ src/BaekJoon/BaekJoon_2338.java
@@ -0,0 +1,16 @@
+package BaekJoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class BaekJoon_2338 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = new BigInteger(sc.nextLine());
+        BigInteger B = new BigInteger(sc.nextLine());
+
+        System.out.println(A.add(B));
+        System.out.println(A.subtract(B));
+        System.out.println(A.multiply(B));
+    }
+}
