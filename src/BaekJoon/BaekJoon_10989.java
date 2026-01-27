503f4f6dfdfd8cfc90db31c4be0bfa2ef6156a96
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 23:08:10 +0900

    10989

---
+++ src/BaekJoon/BaekJoon_10989.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.*;
+
+public class BaekJoon_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N= Integer.parseInt(br.readLine());
+        int[] count = new int[10001];
+
+        for(int i=0;i<N;i++){
+            int x = Integer.parseInt(br.readLine());
+            count[x]++;
+        }
+
+        for(int i=1;i<=10000;i++){
+            while(count[i]-- > 0){
+                bw.write(i+"\n");
+            }
+        }
+        bw.flush();
+    }
+}
