2038047568a7b6f5c055339257b4abbb6055e5aa
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 11:04:49 +0900

    10820

---
+++ src/BaekJoon/BaekJoon_10820.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_10820 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            char[] text = sc.nextLine().toCharArray();
+            int S = 0;
+            int D = 0;
+            int N = 0;
+            int B = 0;
+            for (int i = 0; i < text.length; i++) {
+                char now = text[i];
+                if (now == ' ') B++;
+                else if (now >= 'a' && now <= 'z') S++;
+                else if (now >= 'A' && now <= 'Z') D++;
+                else N++;
+            }
+            System.out.println(S + " " + D + "  " + N + " " + B);
+        }
+    }
+}
