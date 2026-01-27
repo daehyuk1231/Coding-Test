53dbc8ae51bae41ef401dbfd1282bc2423597aa1
daehyuk1231 <daehyuk1231@naver.com>
2026-01-27 09:49:32 +0900

    1977

---
+++ src/BaekJoon/BaekJoon_1977.java
@@ -0,0 +1,30 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class BaekJoon_1977 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int M = sc.nextInt();
+        int N = sc.nextInt();
+        int sum = 0;
+        int min = -1;
+        boolean find_min = false;
+        for (int i = 1; i * i <= N; i++) {
+            int temp = i * i;
+            if (temp >= M) {
+                if (!find_min) {
+                    min = temp;
+                    find_min = true;
+                }
+                sum += temp;
+            }
+        }
+        if (min == -1) {
+            System.out.println(min);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
