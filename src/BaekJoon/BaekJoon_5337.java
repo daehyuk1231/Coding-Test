a056744425799e12219d682034120aea099f5602
daehyuk1231 <daehyuk1231@naver.com>
2026-01-28 20:57:45 +0900

    5337

---
+++ src/BaekJoon/BaekJoon_5337.java
@@ -0,0 +1,9 @@
+package BaekJoon;
+
+public class BaekJoon_5337 {
+    public static void main(String[] args) {
+        System.out.println(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
