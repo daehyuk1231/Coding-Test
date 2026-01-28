e6557135182d8685a65ee29e25203c9e58e914da
daehyuk1231 <daehyuk1231@naver.com>
2026-01-28 19:49:54 +0900

    5338

---
+++ src/BaekJoon/BaekJoon_5338.java
@@ -0,0 +1,11 @@
+package BaekJoon;
+
+public class BaekJoon_5338 {
+    public static void main(String[] args) {
+        System.out.println("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+    }
+}
