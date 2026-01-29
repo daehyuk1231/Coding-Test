c72a4e57f3ba8b525e19ca0a2b505399cb55e106
daehyuk1231 <daehyuk1231@naver.com>
2026-01-29 21:46:50 +0900

    14916

--- src/BaekJoon/BaekJoon_14916.java
+++ src/BaekJoon/BaekJoon_14916.java
@@ -10,6 +10,10 @@ public class BaekJoon_14916 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
+        if (N == 1 || N == 3) {
+            System.out.println(-1);
+            return;
+        }
         int cnt5 = N / 5;
         N = N % 5;
         if (N % 2 != 0) {
