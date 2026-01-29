36bb235a626263814c8654a10043504df6656ee9
daehyuk1231 <daehyuk1231@naver.com>
2026-01-29 10:58:08 +0900

    10170

--- src/BaekJoon/BaekJoon_10845.java
+++ src/BaekJoon/BaekJoon_10845.java
@@ -3,9 +3,6 @@ package BaekJoon;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class BaekJoon_10845 {
     public static void main(String[] args) throws IOException {
