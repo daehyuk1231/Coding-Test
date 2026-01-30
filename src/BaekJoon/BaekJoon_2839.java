ca0a466720baf70bf9e191fcfc64dac4b4666ed2
daehyuk1231 <daehyuk1231@naver.com>
2026-01-30 21:16:59 +0900

    2839

---
+++ src/BaekJoon/BaekJoon_2839.java
@@ -0,0 +1,28 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon_2839 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count = N / 5;
+        N %= 5;
+        if (N == 0) {
+            System.out.println(count);
+            return;
+        }
+        for (int i = 0; i < 3 && count - i >= 0; i++) {
+            int temp = N + 5 * i;
+            if (temp % 3 == 0) {
+                count -= i;
+                count += temp / 3;
+                System.out.println(count);
+                return;
+            }
+        }
+        System.out.println(-1);
+    }
+}
