63616e9c420789f885acd60d0a7ec69a7c5973ca
daehyuk1231 <daehyuk1231@naver.com>
2026-01-30 15:34:19 +0900

    2669

---
+++ src/BaekJoon/BaekJoon_2669.java
@@ -0,0 +1,36 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_2669 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[][] map = new int[101][101];
+        for (int i = 0; i < 4; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            for (int x = x1; x < x2; x++) {
+                for (int y = y1; y < y2; y++) {
+                    map[x][y] = 1;
+                }
+            }
+        }
+            int count = 0;
+            for (int x = 1; x <= 100; x++) {
+                for (int y = 1; y <= 100; y++) {
+                    if (map[x][y] == 1) {
+                        count++;
+                    }
+                }
+            }
+        System.out.println(count);
+
+    }
+}
