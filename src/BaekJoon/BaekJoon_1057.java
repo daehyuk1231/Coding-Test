f12a2848bc9bcf41abf5fd5e04617cc3f7a9f0cf
daehyuk1231 <daehyuk1231@naver.com>
2026-02-04 20:49:16 +0900

    1057

---
+++ src/BaekJoon/BaekJoon_1057.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_1057 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int count = 0;
+        while (A != B) {
+            count++;
+            A = (A + 1) / 2;
+            B = (B + 1) / 2;
+        }
+        System.out.println(count);
+    }
+}
