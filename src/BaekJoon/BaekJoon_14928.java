e4ed1a04c1981b7e62c75f6ef78b7bd435f442c2
daehyuk1231 <daehyuk1231@naver.com>
2026-02-07 17:41:27 +0900

    14928

---
+++ src/BaekJoon/BaekJoon_14928.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class BaekJoon_14928 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String N = br.readLine();
+
+        int mod = 20000303;
+        long result = 0;
+
+        for (int i = 0; i < N.length(); i++) {
+            result = (result * 10 + (N.charAt(i) - '0')) % mod;
+        }
+
+        System.out.println(result);
+    }
+}
