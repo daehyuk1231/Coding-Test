0672bcf86e0debf55ec25ae8616705269ab8cef6
daehyuk1231 <daehyuk1231@naver.com>
2026-02-10 12:49:00 +0900

    7891

---
+++ src/BaekJoon/BaekJoon_7891.java
@@ -0,0 +1,20 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon_7891 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        for (int i = 0; i < T; i++) {
+            st = new StringTokenizer(br.readLine());
+            long a = Long.parseLong(st.nextToken());
+            long b = Long.parseLong(st.nextToken());
+            System.out.println(a + b);
+        }
+    }
+}
