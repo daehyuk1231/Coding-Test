29743bd2aac809ebee0ef69dc6489d50126fb70b
daehyuk1231 <daehyuk1231@naver.com>
2026-02-10 12:44:02 +0900

    14581

---
+++ src/BaekJoon/BaekJoon_14581.java
@@ -0,0 +1,15 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BaekJoon_14581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        System.out.println(":fan::fan::fan:\n" +
+                ":fan::" + s + "::fan:\n" +
+                ":fan::fan::fan:");
+    }
+}
