6397d6c3264e03a39b1b6d03aea964e4205d2f88
daehyuk1231 <daehyuk1231@naver.com>
2026-02-11 14:40:00 +0900

    1904

--- src/BaekJoon/BaekJoon_1904.java
+++ src/BaekJoon/BaekJoon_1904.java
@@ -10,10 +10,12 @@ public class BaekJoon_1904 {
         int N = Integer.parseInt(br.readLine());
         int[] D = new int[N + 1];
         D[1] = 1;
-        D[2] = 2;
-        for (int i = 3; i <= N; i++) {
-            D[i] = D[i - 1] + D[i - 2];
-            D[i] = D[i] % 15746;
+        if (N > 1) {
+            D[2] = 2;
+            for (int i = 3; i <= N; i++) {
+                D[i] = D[i - 1] + D[i - 2];
+                D[i] = D[i] % 15746;
+            }
         }
         System.out.println(D[N]);
     }
