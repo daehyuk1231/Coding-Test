00fc9302fcbc30f2e230d76fa9154db99bb09494
daehyuk1231 <daehyuk1231@naver.com>
2026-02-24 20:37:00 +0900

    2012

---
+++ src/BaekJoon/BaekJoon_2012.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BaekJoon_2012 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(A);
+        long sum = 0;
+        for (int i = 0; i < N; i++) {
+            int d = i + 1;
+            sum += Math.abs(A[i] - d);
+        }
+        System.out.println(sum);
+    }
+}
