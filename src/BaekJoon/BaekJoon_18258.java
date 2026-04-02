2714dfa2524870835b8a4d522e61803bf0114e7d
daehyuk1231 <daehyuk1231@naver.com>
2026-04-02 21:56:25 +0900

    18258

---
+++ src/BaekJoon/BaekJoon_18258.java
@@ -0,0 +1,36 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+
+public class BaekJoon_18258 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String command = br.readLine();
+
+            if (command.startsWith("push")) {
+                int x = Integer.parseInt(command.substring(5));
+                queue.offer(x);
+            } else if (command.equals("pop")) {
+                sb.append(queue.isEmpty() ? -1 : queue.poll()).append('\n');
+            } else if (command.equals("size")) {
+                sb.append(queue.size()).append('\n');
+            } else if (command.equals("empty")) {
+                sb.append(queue.isEmpty() ? 1 : 0).append('\n');
+            } else if (command.equals("front")) {
+                sb.append(queue.isEmpty() ? -1 : queue.peek()).append('\n');
+            } else if (command.equals("back")) {
+                sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append('\n');
+            }
+        }
+
+        System.out.print(sb);
+    }
+}
